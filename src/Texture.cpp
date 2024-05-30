f6d5c8054271184b92e84f5da4f7a87ba832588d
Meindert Kempe <mail@mkempe.com>
2024-05-30 14:50:34 +0200

    Check number of channels when loading texture
    Some of our images lack an alpha channel, loading these with GL_RGBA
    causes a buffer overflow. To prevent this check the number of channels
    and use GL_RGB when the texture has only 3 channels.

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,46 +1,46 @@
 #include "texture.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <cstdio>
 #include <iostream>
 
-Texture::Texture(){}
-Texture::~Texture(){}
+Texture::Texture() {}
+Texture::~Texture() {}
 
-void Texture::InitializeFromFile(std::string fileName){
-    glGenTextures(1, &textureID);
-    
-    std::string filePath = "resources/textures/" + fileName;
-    glBindTexture(GL_TEXTURE_2D, textureID); 
+void Texture::InitializeFromFile(std::string fileName) {
+	glGenTextures(1, &textureID);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
+	std::string filePath = "resources/textures/" + fileName;
+	glBindTexture(GL_TEXTURE_2D, textureID);
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    // load and generate the textures
-    i32 width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true);  
-    u8* data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+	// load and generate the textures
+	i32 width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	u8 *data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
+	if (data && (nrChannels == 4 || nrChannels == 3)) {
+		if (nrChannels == 4)
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+			             data);
+		else
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE,
+			             data);
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	} else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 }
 
-void Texture::DestroyTexture(){
-    glDeleteTextures(1, &textureID);
-}
+void Texture::DestroyTexture() { glDeleteTextures(1, &textureID); }
 
-void Texture::ActivateTexture(i32 textureUnit){
-    glActiveTexture(textureUnit);
-    glBindTexture(GL_TEXTURE_2D, textureID); 
+void Texture::ActivateTexture(i32 textureUnit) {
+	glActiveTexture(textureUnit);
+	glBindTexture(GL_TEXTURE_2D, textureID);
 }
-
